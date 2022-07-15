02ec814de59afb23c7c19d764cd44c822ff5ce74
FENGZHIZI <32665642+hanjizhou98@users.noreply.github.com>
2022-07-15 11:01:59 +0800

    20220715

---
+++ src/main/java/com/example/git/controller/GitTestController.java
@@ -0,0 +1,21 @@
+package com.example.git.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: git test
+ * @author: zj
+ * @date: 2022-07-15 10:59
+ */
+@RestController
+@Slf4j
+public class GitTestController {
+
+    @GetMapping("/git")
+    public String git() {
+        return "这是测试代码";
+    }
+
+}
